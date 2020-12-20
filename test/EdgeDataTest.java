030a49cd3cccd030967946ad771959c2a40dc84c
HelenYunes <74502978+HelenYunes@users.noreply.github.com>
2020-12-20 23:38:02 +0200

    Update EdgeDataTest.java

--- test/EdgeDataTest.java
+++ test/EdgeDataTest.java
@@ -1,3 +1,4 @@
+
 package test;
 
 import api.EdgeData;
@@ -8,18 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EdgeDataTest {
 
-    EdgeData testEdge = new EdgeData(0,1,5.56);
-    EdgeData testEdge1 = new EdgeData(0,1,5.56);
-    EdgeData testEdge2 = new EdgeData(0,1,5.55);
-    EdgeData testEdge3 = new EdgeData(0,10,5.56);
+    EdgeData testEdge = new EdgeData(0, 1, 5.56);
+    EdgeData testEdge1 = new EdgeData(0, 1, 5.56);
+    EdgeData testEdge2 = new EdgeData(0, 1, 5.55);
+    EdgeData testEdge3 = new EdgeData(0, 10, 5.56);
 
     Object testObject = new Bootstrap();
 
     @Test
     void testToString() {
-        boolean test = testEdge.toString().equals("Edge: Source= " + 0 + ", Destination= " + 1 + ", Weight= " + 5.56);
+        boolean test = testEdge.toString().equals("[Edge: {Source= " + 0 + ", Destination= " + 1 + ", Weight= " + 5.56 + "}]");
         assertTrue(test);
-        test = testEdge.toString().equals("Edge: Source= " + 0 + ", Destination= " + 1 + ",Weight= " + 5.56);
+        test = testEdge.toString().equals("Edge: Source= " + 1 + ", Destination= " + 5 + ",Weight= " + 5.56);
         assertFalse(test);
     }
 
