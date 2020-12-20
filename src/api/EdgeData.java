268e6d8756189a8cb7c732f39e661de7c5b77303
HelenYunes <74502978+HelenYunes@users.noreply.github.com>
2020-12-20 19:51:55 +0200

    Update EdgeData.java

--- src/api/EdgeData.java
+++ src/api/EdgeData.java
@@ -1,24 +1,31 @@
 package api;
 
-import com.google.gson.annotations.Expose;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * This class represents the set of operations applicable on a
+ * directional edge(src,dest) in a (directional) weighted graph.
+ *
+ * @author boaz.benmoshe
+ */
 public class EdgeData implements edge_data, Serializable {
-    //@Expose
+
     @SerializedName(value = "src")
     private int Source;
-    //  @Expose
     @SerializedName(value = "dest")
     private int Destination;
-    //  @Expose
     @SerializedName(value = "w")
     private double Weight;
     private String Info;
     private int Tag;
 
+    /**
+     * Constructor:
+     */
     public EdgeData(int src, int dest, double weight) {
         this.Source = src;
         this.Destination = dest;
@@ -27,36 +34,71 @@ public class EdgeData implements edge_data, Serializable {
         this.Tag = 0;
     }
 
+    /**
+     * Return the id of the source node of this edge.
+     *
+     * @return Source
+     */
     @Override
     public int getSrc() {
         return this.Source;
     }
 
+    /**
+     * Return the id of the destination node of this edge
+     *
+     * @return Destination
+     */
     @Override
     public int getDest() {
         return this.Destination;
     }
 
+    /**
+     * Return the weight of this edge (positive value).
+     *
+     * @return Weight
+     */
     @Override
     public double getWeight() {
         return this.Weight;
     }
 
+    /**
+     * @return remark (meta data) associated with this edge
+     */
     @Override
     public String getInfo() {
         return this.Info;
     }
 
+    /**
+     * Allows changing the remark (meta data) associated with this edge.
+     *
+     * @param s
+     */
     @Override
     public void setInfo(String s) {
         this.Info = s + "";
     }
 
+    /**
+     * Temporal data (aka color: e,g, white, gray, black)
+     * which can be used be algorithms
+     *
+     * @return
+     */
     @Override
     public int getTag() {
         return this.Tag;
     }
 
+    /**
+     * This method allows setting the "tag" value for temporal marking an edge - common
+     * practice for marking by algorithms.
+     *
+     * @param t - the new value of the tag
+     */
     @Override
     public void setTag(int t) {
         this.Tag = t;
@@ -67,10 +109,6 @@ public class EdgeData implements edge_data, Serializable {
         this.Weight = weight;
     }
 
-    //    @Override
-//    public String toString() {
-//        return "[Edge: {Source= " + this.Source + ", Destination= " + this.Destination + ", Weight= " + this.Weight+"}]";
-//    }
     @Override
     public String toString() {
         return "{" +
